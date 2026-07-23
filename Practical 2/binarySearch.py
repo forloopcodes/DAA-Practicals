f8fb9a19d5aac9ba70b332441e5fd696a33c3cac
forloopcodes <meetnp1706@gmail.com>
2026-07-23 13:58:25 +0530

    Practical 2 - 2

---
+++ Practical 2/binarySearch.py
@@ -0,0 +1,17 @@
+def binarySearch(arr, left, right, key):
+  mid = left + right - 1 // 2
+  if key == arr[mid]:
+    return mid
+  elif key > arr[mid] and mid != right:
+    return binarySearch(arr, mid + 1, right, key)
+  elif key < arr[mid] and mid != left:
+    return binarySearch(arr, left, mid - 1, key)
+  else:
+    return -1
+
+arr = [0, 1, 2, 5, 7, 8]
+n = len(arr)
+for i in range(n):
+  key = arr[i]
+  position = binarySearch(arr, 0, n-1, key)
+  print("found ", key, " at index ", position)
